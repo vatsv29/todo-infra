db070a67c8109ff8337ac6cb18eddf859d7f949f
Ashwini Jha <kr.ashwinikumarjha@gmail.com>
2025-07-06 17:45:51 +0530

    my stp1 details

--- main.tf
+++ main.tf
@@ -15,8 +15,23 @@ provider "azurerm" {
 
 
 
+
 resource "azurerm_resource_group" "vaibhav1" {
 for_each = var.rg_names
   name     = each.value.name
   location = each.value.location
+}
+
+resource "azurerm_storage_account" "stg" {
+  for_each = var.storage_accounts
+  name                     = each.value.name
+  resource_group_name      = azurerm_resource_group.vaibhav1[each.key].name
+  location                 = azurerm_resource_group.vaibhav1[each.key].location
+  account_tier             = each.value.account_tier
+  account_replication_type = each.value.account_replication_type
+
+  tags = {
+    environment = "Terraform"
+  }
+  
 }
